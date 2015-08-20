3b59ce70a1a1675de1ec7d8ff95606d6b437dbe5
Rory Bramwell <rory.bramwell@gmail.com>
2015-08-20 13:52:42 -0500

    Updated the directory path to check for
    Updated the directory path to check for from "/etc/puppet/modules/elasticsearch' to "/etc/puppet/modules/mongodb"

--- mongodb-puppet/install-puppet-modules.sh
+++ mongodb-puppet/install-puppet-modules.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 mkdir -p /etc/puppet/modules;
 
-if [ ! -d /etc/puppet/modules/elasticsearch ]; then
+if [ ! -d /etc/puppet/modules/mongodb ]; then
   puppet module install puppetlabs-mongodb --version 0.10.0
 fi
